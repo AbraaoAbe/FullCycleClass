4006291262a00533b443497c69a86284d2e6b75d
AbraaoAbe <abraao.santos@edu.ufes.br>
2022-12-08 09:52:47 -0300

    initial commit

---
+++ internal/domain/entity/team.go
@@ -0,0 +1,15 @@
+package entity
+
+type Team struct {
+	ID      string
+	Name    string
+	Players []*Player
+}
+
+func NewTeam(id, name string, players []*Player) *Team {
+	return &Team{
+		ID:      id,
+		Name:    name,
+		Players: players,
+	}
+}
